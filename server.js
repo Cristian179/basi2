87152a6467fc5a1501a81769fe189e84cb55eedb
Cristian179 <71390244+Cristian179@users.noreply.github.com>
2020-10-12 17:28:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
     res.send("ajax.html");
 }); */
 
-app.post("https://basi2-22jpnfxn6.vercel.app/", (req, res)=>{
+app.post("https://basi2.vercel.app/", (req, res)=>{
     var data = JSON.stringify(req.body);
     console.log(data);
 
